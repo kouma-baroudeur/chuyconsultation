d6a05688b3a326f43273086f475d5ecd23f67907
King Velly <evestchris@gmail.com>
2021-07-18 21:16:53 +0100

    ajout baroud

--- app/views/medecins/add-consultation.php
+++ app/views/medecins/add-consultation.php
@@ -14,50 +14,6 @@
             <div class="row">
                 <div class="col-lg-9 col-12 mx-auto">
                     <form action="<?= URLROOT ?>/medecins/ajouterConsultation" method="post" onsubmit="return getContenu();">
-<<<<<<< HEAD
-                        <div class="card card-body mt-4">
-                            <h6 class="mb-0">Nouvelle consultaion</h6>
-                            <p class="text-sm mb-0">Veuillez remplir les champs</p>
-                            <hr class="horizontal dark my-3">
-                            <label for="projectName" class="form-label">Patient</label>
-
-                            <input type="text" class="form-control" name="medecin" value="<?= $data['medecin']->codeMedecin ?>" hidden>
-
-                            <select class="form-control" name="patient">
-                                <option value="Choice 1" selected>Patient <?= $data['idPatient'] ?></option>
-                                <option value="Choice 2">Patient 2</option>
-                                <option value="Choice 3">Patient 3</option>
-                                <option value="Choice 4">Patient 4</option>
-                            </select>
-                            <label class="mt-4">Description de la consultaion</label>
-
-                            <div id="contenuText" class="h-100">
-                                <p>Hello World!</p>
-                                <p>Some initial <strong>bold</strong> text</p>
-                                <p><br></p>
-                            </div>
-                            <input type="text" class="form-control" id="contenu" hidden>
-                            <label class="mt-4 form-label">Symptômes du Patient</label>
-                            <select class="form-control" name="symptomes" id="choices-multiple-remove-button" multiple>
-                                <option value="Choice 1" selected>symptome 1</option>
-                                <option value="Choice 2">symptome 2</option>
-                                <option value="Choice 3">symptome 3</option>
-                                <option value="Choice 4">symptome 4</option>
-                            </select>
-                            <div class="row">
-                                <div class="col-6">
-                                    <label class="form-label">Date de consultaion: </label><span class="text-sm">&nbsp;&nbsp;<?php echo date('d/m/Y'); ?></span>
-                                </div>
-                                <label class="mt-4 form-label">Ajouter des documents</label>
-                                <div class="form-control dropzone" id="dropzone">
-                                    <div class="fallback">
-                                        <input name="file" type="file" multiple />
-                                    </div>
-                                </div>
-                                <div class="d-flex justify-content-end mt-4">
-                                    <button type="submit" name="button" class="btn bg-gradient-info m-0">Ajouter la Consultation</button>
-                                    <button type="button" name="button" class="btn btn-light m-0  ms-2">Cancel</button>
-=======
                     <div class="card card-body mt-4">
                         <h6 class="mb-0">Nouvelle consultaion</h6>
                         <p class="text-sm mb-0">Veuillez remplir les champs</p>
@@ -102,7 +58,6 @@
                             <div class="form-control dropzone" id="dropzone">
                                 <div class="fallback">
                                     <input name="file" type="file" multiple />
->>>>>>> 35f365630677a0597512a1893dd5087186df3859
                                 </div>
                             </div>
                         </div>
